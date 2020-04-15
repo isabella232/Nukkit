ecc2d240b020c38170affa9f8df7314f612cf2dc
Orange <cz335360@gmail.com>
2020-04-15 23:43:14 +0800

    fix TextPacket (thirdPartyName != xboxUserId)

--- src/main/java/cn/nukkit/network/protocol/TextPacket.java
+++ src/main/java/cn/nukkit/network/protocol/TextPacket.java
@@ -19,19 +19,21 @@ public class TextPacket extends DataPacket {
     public static final byte TYPE_CHAT = 1;
     public static final byte TYPE_TRANSLATION = 2;
     public static final byte TYPE_POPUP = 3;
-    public static final byte TYPE_JUKEBOX_POPUP = 4;
-    public static final byte TYPE_TIP = 5;
-    public static final byte TYPE_SYSTEM = 6;
-    public static final byte TYPE_WHISPER = 7;
-    public static final byte TYPE_ANNOUNCEMENT = 8;
+//    public static final byte TYPE_JUKEBOX_POPUP = 4;
+    public static final byte TYPE_TIP = 4;
+    public static final byte TYPE_SYSTEM = 5;
+    public static final byte TYPE_WHISPER = 6;
+    public static final byte TYPE_ANNOUNCEMENT = 7;
     public static final byte TYPE_JSON = 9;
 
     public byte type;
     public String source = "";
     public String message = "";
+    public String thirdPartyName = "";
+    public int platformId = 0;
     public String[] parameters = new String[0];
     public boolean isLocalized = false;
-    public String xboxUserId = "";
+//    public String xboxUserId = "";
     public String platformChatId = "";
 
     @Override
@@ -39,10 +41,13 @@ public class TextPacket extends DataPacket {
         this.type = (byte) getByte();
         this.isLocalized = this.getBoolean() || type == TYPE_TRANSLATION;
         switch (type) {
+            case TYPE_POPUP:
             case TYPE_CHAT:
             case TYPE_WHISPER:
             case TYPE_ANNOUNCEMENT:
                 this.source = this.getString();
+                this.thirdPartyName = this.getString();
+                this.platformId = this.getVarInt();
             case TYPE_RAW:
             case TYPE_TIP:
             case TYPE_SYSTEM:
@@ -51,8 +56,8 @@ public class TextPacket extends DataPacket {
                 break;
 
             case TYPE_TRANSLATION:
-            case TYPE_POPUP:
-            case TYPE_JUKEBOX_POPUP:
+//            case TYPE_POPUP:
+//            case TYPE_JUKEBOX_POPUP:
                 this.message = this.getString();
                 int count = (int) this.getUnsignedVarInt();
                 this.parameters = new String[count];
@@ -60,7 +65,7 @@ public class TextPacket extends DataPacket {
                     this.parameters[i] = this.getString();
                 }
         }
-        this.xboxUserId = this.getString();
+//        this.xboxUserId = this.getString();
         this.platformChatId = this.getString();
     }
 
@@ -70,10 +75,13 @@ public class TextPacket extends DataPacket {
         this.putByte(this.type);
         this.putBoolean(this.isLocalized || type == TYPE_TRANSLATION);
         switch (this.type) {
+            case TYPE_POPUP:
             case TYPE_CHAT:
             case TYPE_WHISPER:
             case TYPE_ANNOUNCEMENT:
                 this.putString(this.source);
+                this.putString(this.thirdPartyName);
+                this.putVarInt(this.platformId);
             case TYPE_RAW:
             case TYPE_TIP:
             case TYPE_SYSTEM:
@@ -82,15 +90,15 @@ public class TextPacket extends DataPacket {
                 break;
 
             case TYPE_TRANSLATION:
-            case TYPE_POPUP:
-            case TYPE_JUKEBOX_POPUP:
+//            case TYPE_POPUP:
+//            case TYPE_JUKEBOX_POPUP:
                 this.putString(this.message);
                 this.putUnsignedVarInt(this.parameters.length);
                 for (String parameter : this.parameters) {
                     this.putString(parameter);
                 }
         }
-        this.putString(this.xboxUserId);
+//        this.putString(this.xboxUserId);
         this.putString(this.platformChatId);
     }
 }
